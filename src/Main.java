be861745e04c2c19e3b7dc393726b3ecf7e36f53
maxkzb <s31766@pjwstk.edu.pl>
2024-10-07 11:02:03 +0200

    Fixed typo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // TODO: we need to add the missing classes!
 
-// OK, I will add `Adder` and s35457 will add `Subtractor`.
+// OK, I will add `Adder` and s30457 will add `Subtractor`.
 
 public class Main {
     public static void main(String[] args) {
